4a8e685d13d8b427c12d83297f0dd81e86f7bede
Excla1mer <47521554+Excla1mer@users.noreply.github.com>
2019-02-18 16:43:23 +0700

    Add files via upload

--- lab1.cpp
+++ lab1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <math.h>
 
 using namespace std;
@@ -6,41 +7,95 @@ using namespace std;
 
 int main()
 {
-	int n,i,j;
-	double a[10][10];
-	double t;
+	int n,i,j,rememb_str,rememb_elem;
+	bool f=true;
+	double a[10][10],Mat_x[100];
+	double t,max;
+	ifstream in("input.txt");
 	cin>>n;
 	for(i=0;i<n;i++)
 	{
 		for(j=0;j<n+1;j++)
 		{
-			cin>> a[i][j];
+			in >> a[i][j];
 		}
 	}	
-	for(i=1;i<n;i++)
+	for(i=0;i<n;i++)
 	{	
 		for(j=0;j<n;j++)
 		{
-			if(j-i<0)
+			if(i==j&&a[i][j]==0)
 			{
-				t=a[i][j]/a[j][j];
-				cout<<t<<" ";
-				for(int z=j;z<n+1;z++)
+				max=a[i][j];
+				for(int z=i+1;z<n;z++)
 				{
-					a[i][z]=a[i][z]-(a[j][z]*t);
+					if(max<a[z][j])
+					{
+						max=a[z][j];
+						rememb_str=z;
+					}
 				}
-			}	
+				cout<<"|"<<max<<"|"<<rememb_str<<"|\n";
+				for(int z=0;z<n+1;z++)
+				{
+					rememb_elem=a[i][z];
+					a[i][z]=a[rememb_str][z];
+					a[rememb_str][z]=rememb_elem;
+				}
+			}
+				if(j-i<0)
+				{
+					t=a[i][j]/a[j][j];
+					for(int z=j;z<n+1;z++)
+					{
+						a[i][z]=a[i][z]-(a[j][z]*t);
+					}
+				}	
 		}
 	}
-	cout<<"\n";
-	for(i=0;i<n;i++)
+	for(i=n-1;i>=0;i--)
 	{
-		for(j=0;j<n+1;j++)
+		Mat_x[i]=a[i][n]/a[i][i];
+		cout<<"{"<<a[i][n]<<","<<a[i][i]<<"}";
+		cout<<"|"<<Mat_x[i]<<"|";
+		for(j=n-1;j>i;j--)
 		{
-			cout<<a[i][j]<<"\t";
+			cout<<"\n";
+			cout<<"{"<<Mat_x[i]<<","<<a[i][j]<<","<<Mat_x[j]<<","<<a[i][i]<<"}";
+			Mat_x[i]=Mat_x[i]-a[i][j]*Mat_x[j]/a[i][i];
+			cout<<"<"<<Mat_x[i]<<">";
 		}
 		cout<<"\n";
 	}
+	cout<<"\n";
+	for(int z=0;z<n;z++)
+	{
+		if(a[z][z]==0)
+		{
+			f=false;
+		}
+	}
+	if(f==true)
+	{
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<n+1;j++)
+			{
+				cout<<a[i][j]<<"\t";
+			}
+			cout<<"\n";
+		}
+		cout<<"\n";
+		for(i=0;i<n;i++)
+		{
+			cout<<Mat_x[i]<<"\t";
+		}
+	}
+	else
+	{
+		cout<<"Matricha virogdennaya\n";
+	}
+		
 	return 0;
 }
 
